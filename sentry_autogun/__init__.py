138008944b74e53cb2174ea007b473f02d94dbe3
Geoffrey LEHEE <g.lehee@jurismarches.com>
2013-11-06 12:19:24 +0100

    Added version and author in __init__

--- sentry_autogun/__init__.py
+++ sentry_autogun/__init__.py
@@ -1,14 +1,3 @@
 # ~*~ coding: utf-8 ~*~
-"""
-sentry_autogun
-~~~~~~~~~~~~~~~~~~
-
-:copyright: (c) 2013 by Geoffrey Lehée
-:license: BSD, see LICENSE for more details.
-"""
-
-try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution('sentry-autogun').version
-except Exception, e:
-    VERSION = 'unknown'
+__author__ = "Geoffrey Lehée <hello@socketubs.org>"
+__version__ = "0.1.3"
